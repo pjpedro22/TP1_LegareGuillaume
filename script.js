7cb8c874ce088d226043479b538e7706525f1f0a
Guillaume Legare <g23leg@gmail.com>
2023-06-10 22:34:15 -0400

    js attempt

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const form = document.getElementById('job');
+const lastName = document.getElementById('lname');
+const firstName = document.getElementById('fname');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +33,65 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+//validation
+
+const validateForm = () => {
+  let noError = true;
+  const lastnameValue = lastName.value.trim();
+  const firstnameValue = firstName.value.trim();
+  const emailValue = email.value.trim();
+  const messageValue = message.value.trim();
+  console.log('validateForm!')
+
+  if (lastnameValue === '') {
+    setError(lastName, 'Lastname is required');
+    noError = false;
+  } else {
+    setSuccess(lastName);
+  }
+  
+  if (firstnameValue === '') {
+    setError(firstName, 'Firstname is required');
+    noError = false;
+  } else {
+    setSuccess(firstName);
+  }
+
+  if (emailValue === '') {
+    setError(email, 'Email is required');
+    noError = false;
+  } else if (!validateEmail(emailValue)) {
+    setError(email, 'Provide a valid email address');
+    noError = false
+  } else {
+    setSuccess(email);
+  }
+
+  return noError;
+}
+
+const validateEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(email).toLowerCase());
+}
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querrySelector('errorMessage');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querrySelector('errorMessage');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+}
+
