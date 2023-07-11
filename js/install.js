174a424719a48796fa463bbe5301dcb92bf38608
Guillaume Legare <g23leg@gmail.com>
2023-07-10 20:54:08 -0400

    add button install

---
+++ js/install.js
@@ -0,0 +1,40 @@
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', installPWA);
+
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+
+
+function saveBeforeInstallPromptEvent(evt) {
+  deferredInstallPrompt = evt;
+  installButton.removeAttribute('hidden');
+
+}
+
+/**
+* Event handler for butInstall - Does the PWA installation.
+*
+* @param {Event} evt
+*/
+function installPWA(evt) {
+  deferredInstallPrompt.prompt();
+  evt.srcElement.setAttribute('hidden', true);
+
+  deferredInstallPrompt.userChoice
+    .then((choice) => {
+      if (choice.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt', choice);
+      } else {
+        console.log('User dismissed the A2HS prompt', choice);
+      }
+      deferredInstallPrompt = null;
+    });
+
+}
+  
+window.addEventListener('appinstalled', logAppInstalled);
+
+function logAppInstalled(evt) {
+  console.log('Weather App was installed.', evt);
+}
