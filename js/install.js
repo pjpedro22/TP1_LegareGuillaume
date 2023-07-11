241900e3c55672d721fe6c65a810832fda775f6f
Guillaume Legare <g23leg@gmail.com>
2023-07-10 21:08:18 -0400

    btn fix

--- js/install.js
+++ js/install.js
@@ -4,19 +4,12 @@ installButton.addEventListener('click', installPWA);
 
 window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
-
-
 function saveBeforeInstallPromptEvent(evt) {
   deferredInstallPrompt = evt;
   installButton.removeAttribute('hidden');
 
 }
 
-/**
-* Event handler for butInstall - Does the PWA installation.
-*
-* @param {Event} evt
-*/
 function installPWA(evt) {
   deferredInstallPrompt.prompt();
   evt.srcElement.setAttribute('hidden', true);
